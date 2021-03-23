9363237ac61f59cfe4563bb11909b26d170d3bac
jun-sung <jun@junsung.io>
2021-03-23 12:34:30 -0700

    Added files via upload

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -36,9 +36,18 @@ const App = () => {
   const course = {
     name: 'Half Stack application development',
     parts: [
-      {name: 'Fundamentals of React', exercise: 10},
-      {name: 'Using props to pass data', exercise: 7},
-      {name: 'State of a component', exercise: 14}
+      {
+        name: 'Fundamentals of React', 
+        exercise: 10
+      },
+      {
+        name: 'Using props to pass data', 
+        exercise: 7
+      },
+      {
+        name: 'State of a component', 
+        exercise: 14
+      }
     ]
   }
 
