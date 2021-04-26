d3d40bcc5e93bfe63ca5a6213ab8a42859892d82
jun-sung <jun@junsung.io>
2021-04-26 14:06:27 -0700

    Added unicafe 1.6 working draft

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -8,51 +8,46 @@ const Results = () =>
 
 const Display = props => 
   <div>{props.text} {props.value}</div>
-  
 
-  const Button = (props) => (
+const Button = (props) => (
   <button onClick={props.handleClick}>
     {props.text}
   </button>
 )
 
 const App = () => {
-  const [clicks, setClicks] = useState({
-    good: 0, neutral: 0, bad: 0, all: 0
-  })
-  
-  const setGood = () =>
-    setClicks({ ...clicks, good: clicks.good + 1, all: clicks.all + 1})
-  
-  const setNeutral = () =>
-    setClicks({ ...clicks, neutral: clicks.neutral + 1, all: clicks.all + 1}) 
-  
-  const setBad = () =>
-    setClicks({ ...clicks, bad: clicks.bad + 1, all: clicks.all - 1})
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [allClicks, setAll] = useState([])
 
-  const setToGood = newValue => {
-    setGood(newValue)
+  const handleGoodClick = () => {
+    setAll(allClicks.concat('G'))
+    setGood(good + 1)
   }
 
-  const setToNeutral = newValue => {
-    setNeutral(newValue)
+  const handleNeutralClick = () => {
+    setAll(allClicks.concat('N'))
+    setNeutral(neutral + 1)
   }
 
-  const setToBad = newValue => {
-    setBad(newValue)
+  const handleBadClick = () => {
+    setAll(allClicks.concat('B'))
+    setBad(bad + 1)
   }
 
   return (
     <>
       <Header />
-      <Button handleClick={setToGood} text="good" />
-      <Button handleClick={setToNeutral} text="neutral" />
-      <Button handleClick={setToBad} text="bad" />
+      <Button handleClick={handleGoodClick} text="good" />
+      <Button handleClick={handleNeutralClick} text="neutral" />
+      <Button handleClick={handleBadClick} text="bad" />
       <Results />
-      <Display value={clicks.good} text="good" />
-      <Display value={clicks.neutral} text="neutral" />
-      <Display value={clicks.bad} text="bad" />
-      <Display value={clicks.all} text="average" />
+      <Display value={good} text="good" />
+      <Display value={neutral} text="neutral" />
+      <Display value={bad} text="bad" />
+      <Display value={allClicks.length} text="all" />
+      <Display value={allClicks.avg} text="average" />
     </>
   )
 }
