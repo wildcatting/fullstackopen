b5b462b9c12b4aa92452d5090020c3af7dfc53e3
jun-sung <staringcat@gmail.com>
2021-04-26 20:46:52 -0700

    Added unicafe step2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -7,7 +7,7 @@ const Results = () =>
   <h2>statistics</h2>
 
 const Display = props => 
-  <div>{props.text} {props.value}</div>
+  <div>{props.text} {props.value} {props.symbol}</div>
 
 const Button = (props) => (
   <button onClick={props.handleClick}>
@@ -19,35 +19,33 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [allClicks, setAll] = useState([])
 
-  const handleGoodClick = () => {
-    setAll(allClicks.concat('G'))
+  const goodClick = () =>
     setGood(good + 1)
-  }
 
-  const handleNeutralClick = () => {
-    setAll(allClicks.concat('N'))
+  const neutralClick = () =>
     setNeutral(neutral + 1)
-  }
 
-  const handleBadClick = () => {
-    setAll(allClicks.concat('B'))
+  const badClick = () =>
     setBad(bad + 1)
-  }
+
+  const Total = good + neutral + bad
+  const Average = (good - bad) / Total
+  const Positive = good / Total * 100
 
   return (
     <>
       <Header />
-      <Button handleClick={handleGoodClick} text="good" />
-      <Button handleClick={handleNeutralClick} text="neutral" />
-      <Button handleClick={handleBadClick} text="bad" />
+      <Button handleClick={goodClick} text="good" />
+      <Button handleClick={neutralClick} text="neutral" />
+      <Button handleClick={badClick} text="bad" />
       <Results />
       <Display value={good} text="good" />
       <Display value={neutral} text="neutral" />
       <Display value={bad} text="bad" />
-      <Display value={allClicks.length} text="all" />
-      <Display value={allClicks.avg} text="average" />
+      <Display value={Total} text="all" />
+      <Display value={Average} text="average" />
+      <Display value={Positive} text="positive" symbol="%" />
     </>
   )
 }
