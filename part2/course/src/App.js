d705efa698e1ce4a1d69ef1fa22a73b895daaa52
jun-sung <staringcat@gmail.com>
2021-05-03 20:41:43 -0700

    Added placeholder course info step6

---
+++ part2/course/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Note from './components/Course'
+
+const App = ({ notes }) => {
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => 
+            <Note key={note.id} note={note} />
+        )}
+      </ul>
+    </div>
+  )
+}
+
+export default App
