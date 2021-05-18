6a44deefeb1b11efedc888a0a544f978aba342ad
jun-sung <jun@junsung.io>
2021-05-18 13:34:03 -0700

    Added phonebook step3

--- part2/phonebook/src/components/Note.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Note = ({ note }) => {
-  return (
-    <p>{note.name}</p>
-  )
-}
-
-export default Note
---
+++ part2/phonebook/src/components/Person.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Person = ({ person }) => {
+  return (
+    <p>{person.name} {person.number}</p>
+  )
+}
+
+export default Person
