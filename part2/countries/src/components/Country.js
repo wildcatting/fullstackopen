c10fd35bd01556043e2e5af8eca444bfd30b245d
Restyled.io <commits@restyled.io>
2021-05-29 01:23:12 +0000

    Restyled by prettier

--- part2/countries/src/components/Country.js
+++ part2/countries/src/components/Country.js
@@ -1,39 +1,45 @@
-import axios from 'axios'
-import React, {useEffect, useState} from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
-import CountryView from './CountryView'
+import CountryView from "./CountryView";
 
-const Country = ({country, length, handleClick}) => {
-  const [weather, setWeather] = useState('')
+const Country = ({ country, length, handleClick }) => {
+  const [weather, setWeather] = useState("");
 
-  const api_key = process.env.REACT_APP_API_KEY
+  const api_key = process.env.REACT_APP_API_KEY;
 
   useEffect(() => {
-    console.log('contacting weatherstack')
+    console.log("contacting weatherstack");
     axios
-      .get(`http://api.weatherstack.com/current?access_key=${api_key}&query=${country.capital}`)
-      .then(response => {
-        console.log('2nd promise fulfilled')
-        setWeather(response.data)
-    })
-  }, [api_key, country.capital])
-  console.log('rendering weather for', country.capital, weather)
-  
-  let show = true
+      .get(
+        `http://api.weatherstack.com/current?access_key=${api_key}&query=${country.capital}`
+      )
+      .then((response) => {
+        console.log("2nd promise fulfilled");
+        setWeather(response.data);
+      });
+  }, [api_key, country.capital]);
+  console.log("rendering weather for", country.capital, weather);
 
-  const clickToShow = show
-    ? <div key={country.name}>
-        {country.name}
-        <button onClick={handleClick} id={country.name}>show</button>
-        <br />
-      </div>
-    : <CountryView country={country} weather={weather} id={country.name} />
+  let show = true;
 
-  return (
-    length === 1
-      ? <CountryView country={country} weather={weather} />
-      : clickToShow
-  )
-}
+  const clickToShow = show ? (
+    <div key={country.name}>
+      {country.name}
+      <button onClick={handleClick} id={country.name}>
+        show
+      </button>
+      <br />
+    </div>
+  ) : (
+    <CountryView country={country} weather={weather} id={country.name} />
+  );
 
-export default Country
+  return length === 1 ? (
+    <CountryView country={country} weather={weather} />
+  ) : (
+    clickToShow
+  );
+};
+
+export default Country;
--- part2/countries/src/components/DisplayCountries.js
+++ part2/countries/src/components/DisplayCountries.js
@@ -1,39 +1,42 @@
-import axios from 'axios'
-import React, {useEffect, useState} from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
-import Country from './Country'
+import Country from "./Country";
 
-const DisplayCountries = ({filter, handleClick}) => {
-  const [countries, setCountries] = useState([])
+const DisplayCountries = ({ filter, handleClick }) => {
+  const [countries, setCountries] = useState([]);
 
   useEffect(() => {
-    console.log('contacting restcountries')
-    axios
-      .get('https://restcountries.eu/rest/v2/all')
-      .then(response => {
-        console.log('promise fulfilled')
-        setCountries(response.data)
-      })
-  }, [])
-  console.log('rendering', countries.length, 'countries')
+    console.log("contacting restcountries");
+    axios.get("https://restcountries.eu/rest/v2/all").then((response) => {
+      console.log("promise fulfilled");
+      setCountries(response.data);
+    });
+  }, []);
+  console.log("rendering", countries.length, "countries");
 
-  const countriesToShow = 
-  filter === ''
-    ? countries.filter(country => country.name === '')
-    : countries.filter((country) => 
-      country.name.toLowerCase().includes(filter.toLowerCase()))
-  console.log('search filter:', countriesToShow)
-
+  const countriesToShow =
+    filter === ""
+      ? countries.filter((country) => country.name === "")
+      : countries.filter((country) =>
+          country.name.toLowerCase().includes(filter.toLowerCase())
+        );
+  console.log("search filter:", countriesToShow);
 
   return (
     <>
       {countriesToShow.length > 10
-        ? 'Too many matches, specify another filter'
-        : countriesToShow.map(country =>
-          <Country key={country.name} country={country} length={countriesToShow.length} handleClick={handleClick} />
-      )}
+        ? "Too many matches, specify another filter"
+        : countriesToShow.map((country) => (
+            <Country
+              key={country.name}
+              country={country}
+              length={countriesToShow.length}
+              handleClick={handleClick}
+            />
+          ))}
     </>
-  )
-}
+  );
+};
 
-export default DisplayCountries
+export default DisplayCountries;
