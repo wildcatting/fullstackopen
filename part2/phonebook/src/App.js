0758933da55b659e000c4cac582807b6a62c3038
Restyled.io <commits@restyled.io>
2021-06-04 21:18:10 +0000

    Restyled by prettier

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,104 +1,113 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from "react";
 
-import Filter from './components/Filter'
-import Notification from './components/Notification'
-import PersonForm from './components/PersonForm'
-import Persons from './components/Persons'
-import phoneService from './services/persons'
+import Filter from "./components/Filter";
+import Notification from "./components/Notification";
+import PersonForm from "./components/PersonForm";
+import Persons from "./components/Persons";
+import phoneService from "./services/persons";
 
-const App =
-    () => {
-      const [persons, setPersons] = useState([])
-      const [newName, setNewName] = useState('')
-      const [newNumber, setNewNumber] = useState('')
-      const [filter, setFilter] = useState('')
-      const [errorMessage, setErrorMessage] = useState(null)
-      const [color, setColor] = useState('')
+const App = () => {
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [color, setColor] = useState("");
 
   useEffect(() => {
-    console.log('effect')
-    phoneService
-      .getAll()
-      .then(initialPersons => {
-        console.log('promise fulfilled')
-        setPersons(initialPersons)
-      })
-  }, [])
+    console.log("effect");
+    phoneService.getAll().then((initialPersons) => {
+      console.log("promise fulfilled");
+      setPersons(initialPersons);
+    });
+  }, []);
 
   const addPerson = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     const newPerson = {
-      name : newName,
-      number : newNumber,
-    }
+      name: newName,
+      number: newNumber,
+    };
 
-    const existingEntry = persons.find((person) => person.name === newName)
+    const existingEntry = persons.find((person) => person.name === newName);
 
     if (existingEntry) {
-      if (window.confirm(`${
-              newName} is already added to phonebook, replace the old number with a new one?`))
+      if (
+        window.confirm(
+          `${newName} is already added to phonebook, replace the old number with a new one?`
+        )
+      )
         phoneService
           .update(existingEntry.id, newPerson)
-          .then(returnedPerson => {
-            setPersons(persons.map((person) => person.id !== returnedPerson.id ? person : returnedPerson))
-        setErrorMessage(`${existingEntry.name}'s number was updated`)
-        setColor('green')
-        setNewName('')
-            setNewNumber('')
+          .then((returnedPerson) => {
+            setPersons(
+              persons.map((person) =>
+                person.id !== returnedPerson.id ? person : returnedPerson
+              )
+            );
+            setErrorMessage(`${existingEntry.name}'s number was updated`);
+            setColor("green");
+            setNewName("");
+            setNewNumber("");
           })
-          .catch(error => {
-            setErrorMessage(`${existingEntry.name}'s information has already been removed from server`)
-            setColor('red')
-            setPersons(persons.filter(p => p.id !== existingEntry.id))
-          })
-            setTimeout(() => { setErrorMessage(null); }, 3000);
-    }
-    else {
+          .catch((error) => {
+            setErrorMessage(
+              `${existingEntry.name}'s information has already been removed from server`
+            );
+            setColor("red");
+            setPersons(persons.filter((p) => p.id !== existingEntry.id));
+          });
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 3000);
+    } else {
       phoneService
         .create(newPerson)
-        .then(returnedPerson => {
-          setPersons(persons.concat(returnedPerson))
-      setErrorMessage(`Added ${newPerson.name}`)
-      setColor('green')
-      setNewName('')
-          setNewNumber('')
+        .then((returnedPerson) => {
+          setPersons(persons.concat(returnedPerson));
+          setErrorMessage(`Added ${newPerson.name}`);
+          setColor("green");
+          setNewName("");
+          setNewNumber("");
         })
-        .catch(error => {
-        setErrorMessage(`${error.response.data.error}`);
-        console.log(error.response.data);
+        .catch((error) => {
+          setErrorMessage(`${error.response.data.error}`);
+          console.log(error.response.data);
         });
-          setTimeout(() => { setErrorMessage(null); }, 3000);
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 3000);
     }
-  }
+  };
 
   const deletePerson = (id) => {
-    const person = persons.find(p => p.id === id)
+    const person = persons.find((p) => p.id === id);
 
     if (window.confirm(`Delete ${person.name}?`))
-    phoneService.deleteEntry(id).then(
-        () => {phoneService.getAll().then(persons => {setPersons(persons)})})
-  }
+      phoneService.deleteEntry(id).then(() => {
+        phoneService.getAll().then((persons) => {
+          setPersons(persons);
+        });
+      });
+  };
 
   return (
     <>
       <h2>Phonebook</h2>
       <Notification message={errorMessage} result={color} />
-      <Filter setFilter={
-    setFilter} />
+      <Filter setFilter={setFilter} />
       <h3>Add a new</h3>
       <PersonForm
-        addPerson={addPerson} 
-        newName={newName} 
-        setNewName={setNewName} 
+        addPerson={addPerson}
+        newName={newName}
+        setNewName={setNewName}
         newNumber={newNumber}
-        setNewNumber={
-    setNewNumber}
+        setNewNumber={setNewNumber}
       />
       <h3>Numbers</h3>
-      <Persons filter={filter} persons={persons} deletePerson={
-    deletePerson} />
+      <Persons filter={filter} persons={persons} deletePerson={deletePerson} />
     </>
-  )
-    }
+  );
+};
 
-export default App
+export default App;
--- part2/phonebook/src/components/Notification.js
+++ part2/phonebook/src/components/Notification.js
@@ -1,25 +1,21 @@
-import React from 'react'
+import React from "react";
 
-const Notification = ({message, result}) => {
+const Notification = ({ message, result }) => {
   const notificationStyle = {
     color: result,
-    background: 'lightgrey',
+    background: "lightgrey",
     fontSize: 20,
-    borderStyle: 'solid',
+    borderStyle: "solid",
     borderRadius: 5,
     padding: 10,
-    marginBottom: 10
-  }
-  
+    marginBottom: 10,
+  };
+
   if (message === null) {
-    return null
+    return null;
   }
 
-  return (
-    <div style={notificationStyle}>
-      {message}
-    </div>
-  )
-}
+  return <div style={notificationStyle}>{message}</div>;
+};
 
-export default Notification
+export default Notification;
