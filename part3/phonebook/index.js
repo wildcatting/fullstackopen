6f8db01315f9e5744dfd7c96ff02b49f841b730f
jun-sung <jun@junsung.io>
2021-06-09 12:31:21 -0700

    Added phonebook backend step2

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -28,6 +28,13 @@ app.get('/', (request, response) => {
   response.send('<h1>Phonebook backend</h1>')
 })
 
+app.get('/info', (request, response) => {
+  response.send(`
+      <p>Phonebook has info for ${persons.length} people</p>
+      <p>${Date()}</p>
+  `)
+})
+
 app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
