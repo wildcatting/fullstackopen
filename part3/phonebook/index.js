1065f6a13aecfd2ea7e97ed5a8f3c8e85b24ce80
jun-sung <jun@junsung.io>
2021-06-14 11:50:26 -0700

    Added phonebook backend step5

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,26 +1,28 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
   {
-    "id": 1,
-    "name": "Arto Hellas",
-    "number": "040-123456"
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456"
   },
   {
-    "id": 2,
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523"
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523"
   },
   {
-    "id": 3,
-    "name": "Dan Abramov",
-    "number": "12-43-234345"
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345"
   },
   {
-    "id": 4,
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122"
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122"
   }
 ]
 
@@ -55,6 +57,25 @@ app.delete('/api/persons/:id', (request, response) => {
   persons = persons.filter(person => person.id !== id)
 
   response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+
+  if (request.body.name === undefined) {
+    return response.status(400).json({
+      error: "name missing"
+    })
+  }
+
+  const person = {
+    name: request.body.name,
+    number: request.body.number,
+    id: Math.floor(Math.random() * 100000)
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
 })
 
 const PORT = 3001
