caac069bcdfab4dfc3df3c34489d6a512ac1d328
jun-sung <jun@junsung.io>
2021-06-15 11:56:04 -0700

    Added phonebook backend step8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,21 @@
 const express = require('express')
+var morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+app.use(morgan("tiny"));
+
+morgan.token("body", req => JSON.stringify(req.body));
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(requestLogger)
 
 let persons = [
   {
@@ -67,8 +81,6 @@ app.post('/api/persons', (request, response) => {
     })
   }
 
-  const existingEntry = persons.find((person) => person.name === request.body.name);
-
   if (persons.find((person) => person.name === request.body.name)) {
     return response.status(400).json({
       error: "name must be unique"
@@ -85,6 +97,12 @@ app.post('/api/persons', (request, response) => {
 
   response.json(person)
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
