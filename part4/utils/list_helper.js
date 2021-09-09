cc4afd26b6301584d645662359af6e2acee0a9a5
jun-sung <jun@junsung.io>
2021-09-09 14:45:11 -0700

    helper functions and unit tests, step4

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,11 +1,34 @@
 const dummy = blogs => 1
 
-const totalLikes = blogs => blogs.reduce((total, blog) => total + blog.likes, 0)
+const totalLikes = blogs => blogs.reduce((a, b) => a + b.likes, 0)
 
-const favoriteBlog = (blogs) => blogs.reduce((total, blog) => blog.likes > total ? blog.likes : total, blogs[0].likes)
+const favoriteBlog = blogs => blogs.reduce((a, b) =>
+  b.likes > a
+    ? b.likes
+    : a, blogs[0].likes)
+
+const mostBlogs = blogs => {
+  const result = blogs.reduce((a, b) => {
+    let known = a.find(found => found.author === b.author)
+
+    if (!known) {
+      return a.concat({ author: b.author, blogs: 1 })
+    }
+
+    known.blogs++
+    return a
+  }, [])
+
+  return result.reduce((a, b) =>
+    a.blogs > b.blogs
+      ? a
+      : b
+  )
+}
 
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
