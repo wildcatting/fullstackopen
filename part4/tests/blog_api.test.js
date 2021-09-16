bb0350cd16b95da3ea768fb0d70f935d6c13c144
jun-sung <jun@junsung.io>
2021-09-15 17:55:02 -0700

    Blog list expansions, step1

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -14,83 +14,107 @@ beforeEach(async () => {
 
   blogObject = new Blog(helper.initialBlogs[1])
   await blogObject.save()
+}, 10000)
+
+describe('when there is initially some blogs saved', () => {
+  test('blogs returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  }, 100000)
+
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  })
+
+  test('a specific blog is within the returned blogs', async () => {
+    const response = await api.get('/api/blogs')
+
+    const titles = response.body.map(r => r.title)
+    expect(titles).toContain('Thoughts and Ideas')
+  })
+
+  test('unique default identifier of blog posts is _id', async () => {
+    const blogs = await Blog.find({})
+    expect(blogs[0]._id).toBeDefined()
+  })
 })
 
-test('blogs returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-}, 100000)
+describe('addition of a new blog', () => {
+  test('a valid blog can be added', async () => {
+    const newBlog = {
+      title: 'Gilder\'s Daily Prophecy',
+      author: 'George Gilder',
+      url: 'gildersdailyprophecy.com'
+    }
 
-test('all blogs are returned', async () => {
-  const response = await api.get('/api/blogs')
-  expect(response.body).toHaveLength(helper.initialBlogs.length)
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+    const titles = blogsAtEnd.map(b => b.title)
+    expect(titles).toContain('Gilder\'s Daily Prophecy')
+  })
+
+  test('if likes are missing, default likes to 0', async () => {
+    const newBlog = {
+      title:'Man on the Margin',
+      author:'Michael Kendall',
+      url:'https://manonthemargin.com',
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    const addedBlog = await blogsAtEnd.find(b => b.title === 'Man on the Margin')
+    expect(addedBlog.likes).toBe(0)
+  })
+
+  test('if title and url are missing, respond with 400 bad request', async () => {
+    const newBlog = {
+      author:'George Gilder',
+      likes:12
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+  })
 })
 
-test('a specific blog is within the returned blogs', async () => {
-  const response = await api.get('/api/blogs')
+describe('deletion of a blog', () => {
+  test('succeeds with status code 204 if id is valid', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToDelete = blogsAtStart[0]
 
-  const titles = response.body.map(r => r.title)
-  expect(titles).toContain('Thoughts and Ideas')
-})
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .expect(204)
 
-test('unique default identifier of blog posts is _id', async () => {
-  const blogs = await Blog.find({})
-  expect(blogs[0]._id).toBeDefined()
-})
+    const blogsAtEnd = await helper.blogsInDb()
 
-test('a valid blog can be added', async () => {
-  const newBlog = {
-    title: 'Gilder\'s Daily Prophecy',
-    author: 'George Gilder',
-    url: 'gildersdailyprophecy.com'
-  }
+    expect(blogsAtEnd).toHaveLength(
+      helper.initialBlogs.length - 1
+    )
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-
-  const blogsPostPost = await helper.blogsInDb()
-  expect(blogsPostPost).toHaveLength(helper.initialBlogs.length + 1)
-
-  const titles = blogsPostPost.map(b => b.title)
-  expect(titles).toContain('Gilder\'s Daily Prophecy')
-})
-
-test('if likes are missing, default likes to 0', async () => {
-  const newBlog = {
-    title:'Man on the Margin',
-    author:'Michael Kendall',
-    url:'https://manonthemargin.com',
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-
-  const blogsPostPost = await helper.blogsInDb()
-  const addedBlog = await blogsPostPost.find(b => b.title === 'Man on the Margin')
-  expect(addedBlog.likes).toBe(0)
-})
-
-test('if title and url are missing, respond with 400 bad request', async () => {
-  const newBlog = {
-    author:'George Gilder',
-    likes:12
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
-
-  const blogsPostPost = await helper.blogsInDb()
-  expect(blogsPostPost).toHaveLength(helper.initialBlogs.length)
+    const titles = blogsAtEnd.map(b => b.title)
+    expect(titles).not.toContain(blogToDelete.title)
+  })
 })
 
 afterAll(() => {
