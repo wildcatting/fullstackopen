500297753889045b198e867f3e5cb615f48945eb
Restyled.io <commits@restyled.io>
2021-10-21 02:00:25 +0000

    Restyled by clang-format

--- part5/src/App.js
+++ part5/src/App.js
@@ -1,23 +1,18 @@
-import React, { useState, useEffect } from 'react'
+import React, {useEffect, useState} from 'react'
+
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
 
-  useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+  useEffect(() => {blogService.getAll().then(blogs => setBlogs(blogs))}, [])
 
-  return (
-    <div>
-      <h2>login to blogs</h2>
+  return (<div><h2>login to blogs<
+          /h2>
       {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
+        <Blog key={blog.id} blog={blog} />)
+} < /div>
   )
 }
 
--- part5/src/services/blogs.js
+++ part5/src/services/blogs.js
@@ -1,11 +1,12 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
 
-const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
-}
+const getAll =
+    () => {
+      const request = axios.get(baseUrl)
+      return request.then(response => response.data)
+    }
 
-const blogService = { getAll}
+const blogService = {getAll}
 
 export default blogService
