0515f503022ebfe8eaede05042e671f891fe3861
jun-sung <jun@junsung.io>
2021-10-28 18:27:07 -0700

    bloglist frontend, step1

--- part5/src/App.js
+++ part5/src/App.js
@@ -1,9 +1,37 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import Notification from './components/Notification'
 import blogService from './services/blogs'
+import loginService from './services/login' 
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('') 
+  const [user, setUser] = useState(null)
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      
+      window.localStorage.setItem(
+        'loggedNoteappUser', JSON.stringify(user)
+      ) 
+      blogService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,12 +39,45 @@ const App = () => {
     )  
   }, [])
 
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <h2>login to blogs</h2>
+      <div>
+        username
+          <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+          <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>      
+  )
+
   return (
     <div>
-      <h2>login to blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      <Notification message={errorMessage} />
+
+      {user === null 
+        ? loginForm()
+        : <div>
+            <h2>blogs</h2>
+            <p>{user.name} logged-in</p>
+            {blogs.map(blog =>
+              <Blog key={blog.id} blog={blog} />
+            )}
+          </div>
+      }
     </div>
   )
 }
--- part5/src/services/blogs.js
+++ part5/src/services/blogs.js
@@ -1,11 +1,32 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
 
+let token = null
+
+const setToken = newToken => {
+  token = `bearer ${newToken}`
+}
+
 const getAll = () => {
   const request = axios.get(baseUrl)
   return request.then(response => response.data)
 }
 
-const blogService = { getAll}
+const create = async newObject => {
+  const config = {
+    headers: { Authorization: token }
+  }
+  
+  const response = await axios.post(baseUrl, newObject, config)
+  return response.data
+}
+
+const update = (id, newObject) => {
+  const request = axios.put(`${baseUrl}/${id}`, newObject)
+  return request.then(response => response.data)
+}
+
+
+const blogService = { getAll, create, update, setToken }
 
 export default blogService
