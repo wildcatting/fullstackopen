f70302d439d25560101fe12b46622a6b9243115b
Restyled.io <commits@restyled.io>
2021-10-30 20:29:32 +0000

    Restyled by clang-format

--- part5/src/App.js
+++ part5/src/App.js
@@ -1,66 +1,60 @@
-import React, { useState, useEffect } from 'react'
+import React, {useEffect, useState} from 'react'
+
 import Blog from './components/Blog'
 import Notification from './components/Notification'
 import blogService from './services/blogs'
-import loginService from './services/login' 
+import loginService from './services/login'
 
-const App = () => {
-  const [blogs, setBlogs] = useState([])
-  const [errorMessage, setErrorMessage] = useState(null)
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('') 
-  const [user, setUser] = useState(null)
+const App =
+    () => {
+      const [blogs, setBlogs] = useState([])
+      const [errorMessage, setErrorMessage] = useState(null)
+      const [username, setUsername] = useState('')
+      const [password, setPassword] = useState('')
+      const [user, setUser] = useState(null)
 
-  const handleLogin = async (event) => {
-    event.preventDefault()
-    try {
-      const user = await loginService.login({
-        username, password,
-      })
-      
-      window.localStorage.setItem(
-        'loggedNoteappUser', JSON.stringify(user)
-      ) 
-      blogService.setToken(user.token)
-      setUser(user)
-      setUsername('')
-      setPassword('')
-    } catch (exception) {
-      setErrorMessage('Wrong credentials')
-      setTimeout(() => {
-        setErrorMessage(null)
-      }, 5000)
-    }
-  }
+      const handleLogin =
+          async (event) => {
+        event.preventDefault()
+        try {
+          const user = await loginService.login({
+            username,
+            password,
+          })
 
-  const handleLogout = async event => {
-    event.preventDefault()
-    try {
-      window.localStorage.removeItem('loggedBlogAppUser')
-      setUser(null)
-    } catch (exception) {
-      setErrorMessage('ulos kirjautuminen ei onnistu')
-      setTimeout(() => {
-        setErrorMessage(null)
-      }, 5000)
-    }
-  }
+          window.localStorage.setItem('loggedNoteappUser', JSON.stringify(user))
+          blogService.setToken(user.token)
+          setUser(user)
+          setUsername('')
+          setPassword('')
+        } catch (exception) {
+          setErrorMessage('Wrong credentials')
+          setTimeout(() => {setErrorMessage(null)}, 5000)
+        }
+      }
 
+      const handleLogout =
+          async event => {
+        event.preventDefault()
+        try {
+          window.localStorage.removeItem('loggedBlogAppUser')
+          setUser(null)
+        } catch (exception) {
+          setErrorMessage('ulos kirjautuminen ei onnistu')
+          setTimeout(() => {setErrorMessage(null)}, 5000)
+        }
+      }
 
-  useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+      useEffect(() => {blogService.getAll().then(blogs => setBlogs(blogs))}, [])
 
-  useEffect(() => {
-    const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser')
-    if (loggedUserJSON) {
-      const user = JSON.parse(loggedUserJSON)
-      setUser(user)
-      blogService.setToken(user.token)
-    }
-  }, [])
+      useEffect(() => {
+        const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser')
+        if (loggedUserJSON) {
+          const user = JSON.parse(loggedUserJSON)
+          setUser(user)
+          blogService.setToken(user.token)
+        }
+      }, [])
 
   const loginForm = () => (
     <form onSubmit={handleLogin}>
@@ -106,6 +100,6 @@ const App = () => {
       }
     </div>
   )
-}
+    }
 
 export default App
