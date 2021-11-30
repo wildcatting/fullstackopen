6d001eb66cbf56f41647b9045f35768b24517cba
Jun Sung Lee <junsung@juns-mbp.lan>
2021-11-29 18:02:33 -0800

    bloglist end to end testing, step1

---
+++ part5/cypress/integration/blog_app.spec.js
@@ -0,0 +1,16 @@
+describe('Blog app', function() {
+  beforeEach(function() {
+    cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    const user = {
+      name: 'Matti Luukkainen',
+      username: 'mluukkai',
+      password: 'salainen'
+    }
+    cy.request('POST', 'http://localhost:3001/api/users/', user) 
+    cy.visit('http://localhost:3000')
+  })
+
+  it('Login form is shown', function() {
+    cy.contains('login').click()
+  })
+})
